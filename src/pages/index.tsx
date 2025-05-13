b11402a2341d6a3e19e12c070a04e9b5859af1ba
leonardoalbano <leonardoalbanom@yahoo.com.br>
2025-05-13 09:12:38 -0300

    config layout and create logo, footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,8 +14,6 @@ export default function Home() {
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <Header />
-    </div>
+    ></div>
   );
 }
