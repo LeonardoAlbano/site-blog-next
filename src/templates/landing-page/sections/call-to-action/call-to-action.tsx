f00e32c24db374696e0a07ae894c9df716663b78
leonardoalbano <leonardoalbanom@yahoo.com.br>
2025-05-13 15:43:16 -0300

    refatoring landing-page

--- src/templates/landing-page/sections/call-to-action/call-to-action.tsx
+++ src/templates/landing-page/sections/call-to-action/call-to-action.tsx
@@ -1,7 +1,7 @@
 import { ArrowRight, Store } from "lucide-react";
 import { PT_Sans_Caption } from "next/font/google";
 import Link from "next/link";
-import { Button } from "../ui/button";
+import { Button } from "../../../../components/ui/button";
 
 const ptSansCaption = PT_Sans_Caption({
   subsets: ["latin"],
@@ -10,13 +10,15 @@ const ptSansCaption = PT_Sans_Caption({
 
 export function CallToActionSection() {
   return (
-    <section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
-      <div className="container">
-        <div className="flex flex-col items-center gap-6 text-center">
-          <div className="p-4 bg-cyan-300 w-fit rounded-full">
-            <Store className="text-cyan-100" />
-          </div>
+    <section className="relative py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="absolute inset-0 bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
 
+      <div className="absolute top-0 left-1/2 -translate-y-1/2 -translate-x-1/2 p-4 bg-cyan-300 w-fit rounded-full">
+        <Store className="text-cyan-100" />
+      </div>
+
+      <div className="container relative">
+        <div className="flex flex-col items-center gap-6 text-center">
           <h2
             className={`${ptSansCaption.className} text-gray-100 text-balance text-heading-xl`}
           >
